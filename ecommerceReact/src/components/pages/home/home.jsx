f5f0bb8b0b3dbb86017562a95e604b34c13d2705
iAmRamiro <mercadoeze09@gmail.com>
2023-08-01 15:10:01 -0300

    mas errores

--- ecommerceReact/src/components/pages/home/home.jsx
+++ ecommerceReact/src/components/pages/home/home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import SliderContainer from "../../common/Slider/SliderContainer";
 import ItemFeaturedContainer from "./itemFeatured/ItemFeaturedContainer";
 import OffersContainer from "./offers/OffersContainer";
